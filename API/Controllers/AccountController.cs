c48b6d5da96a8ead286ffe1e432ac549eb630cdc
Nikita <nikitaka@edu.hac.ac.il>
2022-04-26 12:23:45 +0300

    End of section 12

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using API.Entities;
 using API.DTOs;
 using API.Interfaces;
+using AutoMapper;
 
 namespace API.Controllers
 {
@@ -16,11 +17,13 @@ namespace API.Controllers
     {
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
+        private readonly IMapper _mapper;
 
-        public AccountController(DataContext context, ITokenService tokenService)
+        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
         {
             _context = context;
             _tokenService = tokenService;
+            _mapper = mapper;
         }
 
         [HttpPost("register")]
@@ -28,22 +31,23 @@ namespace API.Controllers
         {
             if(await UserExists(registerDto.Username)) return BadRequest("Username is already taken");
 
+            var user = _mapper.Map<AppUser>(registerDto);
+
             using var hmac = new HMACSHA512();
 
-            var user = new AppUser
-            {
-                UserName = registerDto.Username.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-                PasswordSalt = hmac.Key
-            };
 
-            _context.Users.Add(user);
+            user.UserName = registerDto.Username.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            user.PasswordSalt = hmac.Key;
+
+                _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
             return new UserDto
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
+                KnownAs = user.KnownAs
             };
         }
 
@@ -69,7 +73,8 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+                KnownAs = user.KnownAs
             };
 
         }
--- client/src/app/_guards/auth.guard.ts
+++ client/src/app/_guards/auth.guard.ts
@@ -12,13 +12,13 @@ export class AuthGuard implements CanActivate {
 
   constructor(private accountService: AccountService, private toastr: ToastrService) {}
 
-  canActivate(): Observable<boolean>{
+  canActivate(): Observable<boolean> {
     return this.accountService.currentUser$.pipe(
       map(user => {
         if (user) return true;
         this.toastr.error('You shall not pass!')
       })
-    )
+    );
   }
   
 }
